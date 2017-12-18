16fc3503c05dc7f6eee5494f85ff5ca885d3d0fd
brandonlund <lundbra@lqpv.net>
2017-12-18 15:53:44 -0600

    fixy fixy

--- src/org/usfirst/frc5638/shiftyTenk/commands/zeroGyro.java
+++ src/org/usfirst/frc5638/shiftyTenk/commands/zeroGyro.java
@@ -2,51 +2,35 @@ package org.usfirst.frc5638.shiftyTenk.commands;
 
 import org.usfirst.frc5638.shiftyTenk.Robot;
 
-import com.kauailabs.navx.frc.AHRS;
-
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
-public class rotate90Command extends Command {
-	
-	public AHRS ahrs;
-	
-    public rotate90Command() {
+public class zeroGyro extends Command {
+
+    public zeroGyro() {
     	requires(Robot.rotateAngle);
-    	
-    	
-    	try{
-        	ahrs = new AHRS(SPI.Port.kMXP);
-        }catch(RuntimeException ex){
-        	DriverStation.reportError("ahrs problem: " + ex.getMessage(), true);
-        }
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	ahrs.zeroYaw();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.rotateAngle.rotate(90);
-    	System.out.println("Angle: " + ahrs.getAngle());
+    	Robot.rotateAngle.zeroGyro();
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.driveTrain.stop();
     }
 
     // Called when another command which requires one or more of the same
--- src/org/usfirst/frc5638/shiftyTenk/subsystems/rotateAngle.java
+++ src/org/usfirst/frc5638/shiftyTenk/subsystems/rotateAngle.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc5638.shiftyTenk.subsystems;
 
+import org.usfirst.frc5638.shiftyTenk.Robot;
 import org.usfirst.frc5638.shiftyTenk.RobotMap;
 import org.usfirst.frc5638.shiftyTenk.commands.*;
 import com.ctre.MotorControl.CANTalon;
@@ -7,6 +8,7 @@ import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SPI;
 
@@ -14,48 +16,63 @@ import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.livewindow.LiveWindowSendable;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class rotateAngle extends Subsystem implements PIDOutput {
+public class rotateAngle extends PIDSubsystem implements PIDOutput {
 	
 	AHRS ahrs;
-	PIDController turnController;
+	public PIDController turnController;
 	double rotateToAngleRate;
 	double currentRotationRate;
 	
 	//Tune this vvvvv
 	
-	static final double kP = 0.03;
-	static final double kI = 0.00;
+	static final double kP = 10;
+	static final double kI = 0.0;
 	static final double kD = 0.00;
-	static final double kF = 0.00;
+	static final double kF = .00;
 	
 	//Tune this ^^^^^
 	//How close the robot will attempt to get to desired heading vvvv
 	
     static final double kToleranceDegrees = 2.0f;
-    
-    private final CANTalon left1 = RobotMap.driveTrainleft;
-    private final CANTalon left2 = RobotMap.driveTrainleft1;
-    private final CANTalon right1 = RobotMap.driveTrainright;
-    private final CANTalon right2 = RobotMap.driveTrainright1;
-    private final RobotDrive robotDrive41 = RobotMap.driveTrainrobotDrive;
+
 
     public rotateAngle() {
+    	super("rotateAngle", 1.0, 0.0, 0.0);
+    	setAbsoluteTolerance(kToleranceDegrees);
+    	getPIDController().setContinuous(true);
+    	LiveWindow.addActuator("rotateAngle", "PIDSubsystem Controller", getPIDController());
+    	
+    	
     	//Communications with the NavX on the MXP SPI Bus
     	try {
             ahrs = new AHRS(SPI.Port.kMXP); 
         } catch (RuntimeException ex ) {
             DriverStation.reportError("Error instantiating navX-MXP:  " + ex.getMessage(), true);
         }
-    	//Communications with the NavX on the MXP SPI Bus
+    	LiveWindow.addSensor("rotateAngle", "Gyro NavX", ahrs);
     	
+    	//Communications with the NavX on the MXP SPI Bus
+    	/*
     	turnController = new PIDController(kP, kI, kD, kF, ahrs, this);
+    	turnController.setPID(kP, kI, kD, kF);
     	turnController.setInputRange(-180.0f, 180.0f);
-    	turnController.setOutputRange(-1.0, 1.0); //motor speed?
+    	turnController.setOutputRange(-.5, .5); //motor speed?
     	turnController.setAbsoluteTolerance(kToleranceDegrees);
     	turnController.setContinuous(true);
     	
-    	LiveWindow.addActuator("Rotate", "RotateSub", turnController);
+    	SmartDashboard.putNumber("Gyro", ahrs.getYaw());
+		SmartDashboard.putNumber("p", turnController.getP());
+    	SmartDashboard.putNumber("i", turnController.getI());
+    	SmartDashboard.putNumber("d", turnController.getD());
+    	SmartDashboard.putNumber("f", turnController.getF());
+    	SmartDashboard.putNumber("error", turnController.getError());
+    	SmartDashboard.putNumber("setpoint", turnController.getSetpoint());
+    	SmartDashboard.putNumber("avg. error all", turnController.getAvgError());
+    	*/
+    	
     }
 
     public void initDefaultCommand() {
@@ -65,16 +82,57 @@ public class rotateAngle extends Subsystem implements PIDOutput {
     	
     }
     
-	@Override
+	
 	public void pidWrite(double output) {
 		// TODO Auto-generated method stub
 		rotateToAngleRate = output;
 	}
-
+/*
 	
-	public void rotate(double angle) {
-		turnController.setSetpoint(angle);
+	public void rotate0() {
+		turnController.setSetpoint(0);
+		Robot.driveTrain.rotateAngle(turnController.get());
 		turnController.enable();
+		
+		SmartDashboard.putNumber("Gyro", ahrs.getYaw());
+		SmartDashboard.putNumber("p", turnController.getP());
+    	SmartDashboard.putNumber("i", turnController.getI());
+    	SmartDashboard.putNumber("d", turnController.getD());
+    	SmartDashboard.putNumber("f", turnController.getF());
+    	SmartDashboard.putNumber("error", turnController.getError());
+    	SmartDashboard.putNumber("setpoint", turnController.getSetpoint());
+    	SmartDashboard.putNumber("avg. error", turnController.getAvgError());
+	}
+	public void rotate90() {
+		turnController.setSetpoint(90);
+		Robot.driveTrain.rotateAngle(turnController.get());
+		turnController.enable();
+		
+		SmartDashboard.putNumber("Gyro", ahrs.getYaw());
+		SmartDashboard.putNumber("p", turnController.getP());
+    	SmartDashboard.putNumber("i", turnController.getI());
+    	SmartDashboard.putNumber("d", turnController.getD());
+    	SmartDashboard.putNumber("f", turnController.getF());
+    	SmartDashboard.putNumber("error", turnController.getError());
+    	SmartDashboard.putNumber("setpoint", turnController.getSetpoint());
+    	SmartDashboard.putNumber("avg. error", turnController.getAvgError());
+	}
+*/
+	public void zeroGyro() {
+		ahrs.reset();
+	}
+
+	@Override
+	protected double returnPIDInput() {
+		// TODO Auto-generated method stub
+		return ahrs.pidGet();
+	}
+
+	@Override
+	protected void usePIDOutput(double output) {
+		// TODO Auto-generated method stub
+		Robot.driveTrain.rotateAngle(output);
+		
 	}
 }
 

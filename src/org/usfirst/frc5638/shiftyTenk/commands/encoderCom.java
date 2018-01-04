04efe7d67c8b7daf07e1588d775328dc2311a0c9
brandonlund <lundbra@lqpv.net>
2018-01-03 20:27:03 -0600

    its a PIDy i had to change some stuff

--- src/org/usfirst/frc5638/shiftyTenk/commands/encoderCom.java
+++ src/org/usfirst/frc5638/shiftyTenk/commands/encoderCom.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc5638.shiftyTenk.commands;
 
 import org.usfirst.frc5638.shiftyTenk.Robot;
+import org.usfirst.frc5638.shiftyTenk.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -8,8 +9,11 @@ import edu.wpi.first.wpilibj.command.Command;
  *
  */
 public class encoderCom extends Command {
+	
+	private float m_setpoint;
 
-    public encoderCom() {
+    public encoderCom(float setpoint) {
+    	m_setpoint = setpoint;
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	requires(Robot.encoder);
@@ -17,6 +21,8 @@ public class encoderCom extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	RobotMap.driveTrainleft.setSetpoint(m_setpoint);
+    	RobotMap.driveTrainright.setSetpoint(m_setpoint);
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -30,10 +36,13 @@ public class encoderCom extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.encoder.done();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	
+    	end();
     }
 }
--- src/org/usfirst/frc5638/shiftyTenk/subsystems/encoder.java
+++ src/org/usfirst/frc5638/shiftyTenk/subsystems/encoder.java
@@ -1,6 +1,9 @@
 package org.usfirst.frc5638.shiftyTenk.subsystems;
 
+import org.usfirst.frc5638.shiftyTenk.Robot;
 import org.usfirst.frc5638.shiftyTenk.RobotMap;
+
+import com.ctre.MotorControl.SmartMotorController.TalonControlMode;
 
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -9,7 +12,6 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class encoder extends Subsystem {
-	public double setpoint;
 	public static double kP = 0.0;
 	public static double kI = 0.0;
 	public static double kD = 0.0;
@@ -17,13 +19,26 @@ public class encoder extends Subsystem {
 	int izone = 100;	//encoder ticks
 	double ramprate = 6;	//volts/sec
 	int profile = 0; //0 or 1
-
+	private double m_outputleft;
+	private double m_outputright;
+	public double setpointleft;
+	public double setpointright;
     // Initialize your subsystem here
     public encoder() {
-    	RobotMap.driveTrainleft.setPID(kP, kI, kD, kF, izone, ramprate, profile);
-    	RobotMap.driveTrainright.setPID(kP, kI, kD, kF, izone, ramprate, profile);
+    	RobotMap.driveTrainleft.changeControlMode(TalonControlMode.Position);
+    	RobotMap.driveTrainright.changeControlMode(TalonControlMode.Position);
     	
-    	RobotMap.driveTrain.SetPosition(1000);
+    	RobotMap.driveTrainleft.setSetpoint(setpointleft);
+    	RobotMap.driveTrainright.setSetpoint(setpointright);
+    	
+    	RobotMap.driveTrainleft.setPID(kP, kI, kD, kF, izone, ramprate, profile); //PID Left
+    	RobotMap.driveTrainright.setPID(kP, kI, kD, kF, izone, ramprate, profile);//PID right
+    	
+    	
+    	m_outputleft = RobotMap.driveTrainleft.pidGet();	//gets PID output for left side.
+    	m_outputright = RobotMap.driveTrainright.pidGet();	//gets PID output for right side.
+    	
+    	
         // Use these to get going:
         // setSetpoint() -  Sets where the PID controller should move the system
         //                  to
@@ -35,15 +50,28 @@ public class encoder extends Subsystem {
         //setDefaultCommand(new MySpecialCommand());
     }
 
-    protected double returnPIDInput() {
+    protected double returnPIDInput(double output) {
+		
         // Return your input value for the PID loop
         // e.g. a sensor, like a potentiometer:
         // yourPot.getAverageVoltage() / kYourMaxVoltage;
-        return 0.0;
+    	return output;
+    	
     }
 
-    protected void usePIDOutput(double output) {
+    protected void usePIDOutput(double outputleft, double outputright) {
+    	outputleft = m_outputleft;
+    	outputright = m_outputright;
         // Use output to drive your system, like a motor
         // e.g. yourMotor.set(output);
+    	RobotMap.driveTrainleft.set(outputleft);
+    	RobotMap.driveTrainright.set(outputright);
+    }
+    
+    public void done() {
+    	RobotMap.driveTrainleft.set(0);
+    	RobotMap.driveTrainright.set(0);
+    	RobotMap.driveTrainleft.changeControlMode(TalonControlMode.PercentVbus);
+    	RobotMap.driveTrainright.changeControlMode(TalonControlMode.PercentVbus);
     }
 }
